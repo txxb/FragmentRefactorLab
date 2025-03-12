64f0f4fb8f8bdd99b8817b80ab64d6f9c490f6a6
Maab Taha <tmaab13@gmail.com>
2025-03-12 16:00:57 -0400

    activity contents replaced with fragment

---
+++ app/src/main/java/edu/temple/fragmentrefactor/NameCollection.kt
@@ -0,0 +1,39 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class NameCollection : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_name_collection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val displayTextView = view.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = view.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = view.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
+    }
+
+}
